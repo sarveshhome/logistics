5c92cc95fcbcc7de18a7b35713a64199ecdf4a1e
sarveshhome <sarvesh.carlton@gmail.com>
2019-03-22 23:06:13 +0530

    https

--- logisticsUI/src/environments/environment.ts
+++ logisticsUI/src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl:'http://192.168.0.23:81/service/',
+  // apiUrl:'https://addev.abc.com/service',
+  isMasterMockTrue: false,
+  wpSSO:'https://addev.abc.com/wordpress/wp-json/wp-single-sign-on/login'
 };
 
 /*
